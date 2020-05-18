be19d40ef9508925dd7de71f185255478b3a9eb4
Mert Nacar <mertnacar2374@gmail.com>
2020-05-18 14:33:36 +0300

    added header and axios

--- src/utils/httpHelper.ts
+++ src/utils/httpHelper.ts
@@ -1,13 +1,14 @@
+import axios from "axios";
 export const get = async (url: any) => {
   try {
-    const res = await fetch(url, {
+    const res = await axios.get(url, {
       headers: {
-        Accept: "application/json"
+        Accept: "application/json",
+        "Accept-Language": "tr",
       },
     });
     if (res.status === 200) {
-      const response = await res.json();
-      return { err: false, data: response };
+      return { err: false, data: res.data };
     } else throw new Error();
   } catch {
     return { err: true };
